bf79ed0338cf8e33e09911c05d9b9dfa2d0a5e28
yidasanqian <yidasanqian@gmail.com>
2024-02-29 15:55:17 +0800

    init commit

---
+++ recognizeTextSample.py
@@ -0,0 +1,56 @@
+
+import os
+from alibabacloud_ocr_api20210707.client import Client as ocr_api20210707Client
+from alibabacloud_tea_openapi import models as open_api_models
+from alibabacloud_ocr_api20210707 import models as ocr_api_20210707_models
+from alibabacloud_tea_util.client import Client as UtilClient
+from alibabacloud_tea_util import models as util_models
+
+access_key = os.getenv('ALIBABA_CLOUD_ACCESS_KEY_ID')
+access_secret = os.getenv('ALIBABA_CLOUD_ACCESS_KEY_SECRET')
+
+def create_client(
+        access_key_id: str,
+        access_key_secret: str,
+    ) -> ocr_api20210707Client:
+        """
+        使用AK&SK初始化账号Client
+        @param access_key_id:
+        @param access_key_secret:
+        @return: Client
+        @throws Exception
+        """
+        config = open_api_models.Config(
+            # 必填，您的 AccessKey ID,
+            access_key_id=access_key_id,
+            # 必填，您的 AccessKey Secret,
+            access_key_secret=access_key_secret
+        )
+        # Endpoint 请参考 https://api.aliyun.com/product/ocr-api
+        config.endpoint = f'ocr-api.cn-hangzhou.aliyuncs.com'
+        return ocr_api20210707Client(config)
+
+def get_ocr_text(url):
+    client = create_client(access_key, access_secret)
+    recognize_all_text_request = ocr_api_20210707_models.RecognizeAllTextRequest(
+            type='General',
+            output_figure=True,
+            output_coordinate=UtilClient.to_bytes('points'),
+            url=url
+        )
+    runtime = util_models.RuntimeOptions()
+    try:
+        # 复制代码运行请自行打印 API 的返回值
+        resp = client.recognize_all_text_with_options(recognize_all_text_request, runtime)
+        print(f"resp:{resp.body}")
+        return resp.body
+    except Exception as error:
+        # 错误 message
+        print(error.message)
+        # 诊断地址
+        print(error.data.get("Recommend"))
+        UtilClient.assert_as_string(error.message)
+
+if __name__ == '__main__':
+    url = "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fsafe-img.xhscdn.com%2Fbw1%2F227bb9d7-99ac-490f-9172-3e332677f6bf%3FimageView2%2F2%2Fw%2F1080%2Fformat%2Fjpg&refer=http%3A%2F%2Fsafe-img.xhscdn.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1711779614&t=2041c9d3969f147f293f1d7218503d36"
+    get_ocr_text(url)       
---
+++ web.py
@@ -0,0 +1,142 @@
+import os
+import inspect
+import json
+from openai import AzureOpenAI
+from recognizeTextSample import get_ocr_text
+
+api_base = os.getenv("AZURE_OPENAI_ENDPOINT")
+api_key= os.getenv("AZURE_OPENAI_API_KEY")
+api_version="2023-12-01-preview",
+model_name = 'gpt-4' #Replace with model deployment name
+
+client = AzureOpenAI(
+    api_key=api_key,  
+    azure_endpoint=api_base,
+    api_version=api_version
+)
+
+tools = [
+    {
+        "type": "function",
+        "function": {
+            "name": "get_ocr_text",
+            "description": "OCR统一识别接口支持识别多种图片类型",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "url": {
+                        "type": "string",
+                        "description": "图片链接（长度不超过 2048，不支持 base64）。示例值:https://example.png",
+                    }
+                },
+                "required": ["url"],
+            },
+        },
+    },
+]
+available_functions = {
+    "get_ocr_text": get_ocr_text
+}
+
+# helper method used to check if the correct arguments are provided to a function
+def check_args(function, args):
+    sig = inspect.signature(function)
+    params = sig.parameters
+
+    # Check if there are extra arguments
+    for name in args:
+        if name not in params:
+            return False
+    # Check if the required arguments are provided
+    for name, param in params.items():
+        if param.default is param.empty and name not in args:
+            return False
+
+    return True
+
+def run_conversation(messages, tools, available_functions):
+    # Step 1: send the conversation and available functions to GPT
+    response = client.chat.completions.create(
+        model=model_name,
+        messages=messages,
+        tools=tools,
+        tool_choice="auto",
+    )
+
+    response_message = response.choices[0].message
+
+    # Step 2: check if GPT wanted to call a function
+    if response_message.tool_calls:
+        print("Recommended Function call:")
+        print(response_message.tool_calls[0])
+        print()
+
+        # Step 3: call the function
+        # Note: the JSON response may not always be valid; be sure to handle errors
+
+        function_name = response_message.tool_calls[0].function.name
+
+        # verify function exists
+        if function_name not in available_functions:
+            return "Function " + function_name + " does not exist"
+        function_to_call = available_functions[function_name]
+
+        # verify function has correct number of arguments
+        function_args = json.loads(response_message.tool_calls[0].function.arguments)
+        if check_args(function_to_call, function_args) is False:
+            return "Invalid number of arguments for function: " + function_name
+        function_response = function_to_call(**function_args)
+
+        print("Output of function call:")
+        print(function_response)
+        print()
+
+        # Step 4: send the info on the function call and function response to GPT
+
+        # adding assistant response to messages
+        messages.append(
+            {
+                "role": response_message.role,
+                "function_call": {
+                    "name": response_message.tool_calls[0].function.name,
+                    "arguments": response_message.tool_calls[0].function.arguments,
+                },
+                "content": None,
+            }
+        )
+
+        # adding function response to messages
+        messages.append(
+            {
+                "role": "tool",
+                "name": function_name,
+                "content": function_response,
+            }
+        )  # extend conversation with function response
+
+        print("Messages in second request:")
+        for message in messages:
+            print(message)
+        print()
+
+        second_response = client.chat.completions.create(
+            messages=messages,
+            model=model_name,
+        )  # get a new response from GPT where it can see the function response
+
+        return second_response
+    return response
+
+
+
+if __name__ == '__main__':
+    messages = [
+     {
+        "role": "system",
+        "content": "Assistant is a helpful assistant that helps users get answers to questions. Assistant has access to several tools and sometimes you may need to call multiple tools in sequence to get answers for your users.",
+        }
+    ]    
+    messages.append({"role": "user", "content": "描述这张图片:url=https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fsafe-img.xhscdn.com%2Fbw1%2F227bb9d7-99ac-490f-9172-3e332677f6bf%3FimageView2%2F2%2Fw%2F1080%2Fformat%2Fjpg&refer=http%3A%2F%2Fsafe-img.xhscdn.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1711779614&t=2041c9d3969f147f293f1d7218503d36"})
+    print("Final Response:")
+    assistant_response = run_conversation(messages, tools, available_functions)
+    print(assistant_response.choices[0].message)
